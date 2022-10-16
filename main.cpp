1cc6c214c09c79573b0fbc1d4f91cc79be36994d
happycloudy <arion.mikola@gmail.com>
2022-10-16 03:55:18 +0300

    refactored to openMP version

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include "vector"
+#include <omp.h>
+#include "utilities/fillArrays.h"
+#include "utilities/printResult.h"
+#include "utilities/play.h"
+
+using namespace std;
+
+int const playAmount = 10000;
+int const playersAmount = 4;
+double const winChance = 0.1;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> winSum;
+
+    fillArrays(winSum, playersAmount);
+
+    omp_set_num_threads(playersAmount);
+
+    #pragma omp parallel default(none) shared(winSum, winChance)
+    play(winSum, omp_get_thread_num(), playAmount, winChance);
+
+    printResult(winSum, playersAmount);
     return 0;
 }
--- utilities/fillArrays.h
+++ utilities/fillArrays.h
@@ -5,17 +5,11 @@
 #ifndef LAB2_FILLARRAYS_H
 #define LAB2_FILLARRAYS_H
 
-#include "thread"
 #include "vector"
-#include "play.h"
 
-void fillArrays(std::vector<int> &winSum, std::vector<std::thread> &players, int playersAmount, int playAmount, double winChance) {
+
+void fillArrays(std::vector<int> &winSum, int playersAmount) {
     for (int i = 0; i < playersAmount; ++i) winSum.push_back(0);
-
-    for (int i = 0; i < playersAmount; ++i) {
-        std::thread player(play, ref(winSum), i, playAmount, winChance);
-        players.push_back(std::move(player));
-    }
 }
 
 
